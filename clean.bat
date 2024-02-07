a0d0fdbb8507fd0555f43a8fd597c9c8822557a9
reincas <r.caspary@posteo.de>
2024-02-07 13:24:10 +0100

    Update of clean.bat

--- clean.bat
+++ clean.bat
@@ -1,4 +1,4 @@
 @echo off
-rmdir build /s /q
-rmdir dist /s /q
-rmdir ScalarDiffractionTools.egg-info /s /q
+if exist build\ rmdir /s /q build 
+if exist dist\ rmdir /s /q dist
+if exist ScalarDiffractionTools.egg-info\ rmdir /s /q ScalarDiffractionTools.egg-info
