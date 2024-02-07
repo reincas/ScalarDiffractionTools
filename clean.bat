aa5662aebb7d542eb699d8110031f45d08d7a64e
reincas <r.caspary@posteo.de>
2024-02-07 23:28:10 +0100

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -2,3 +2,4 @@
 if exist build\ rmdir /s /q build 
 if exist dist\ rmdir /s /q dist
 for /d %%i in (*.egg-info) do rmdir /s /q %%i
+for /d /r %%i in (__pycache__) do ( @if exist %%i rmdir /s /q %%i )
